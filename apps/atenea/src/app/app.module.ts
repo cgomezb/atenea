8a700a92d3889aef43575b33eb3831c8f7aaa7d4
Cesar Gomez <cesargomez280@gmail.com>
2021-11-13 16:34:23 -0500

    feat: CG-0003 Updated app module with core module

--- apps/atenea/src/app/app.module.ts
+++ apps/atenea/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [CoreModule, AppRoutingModule],
   bootstrap: [AppComponent]
 })
 
