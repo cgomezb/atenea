f1b5fc966bc5acb87bb80a98cd88b3be9d9b4e24
Cesar Gomez <cesargomez280@gmail.com>
2021-11-13 16:24:08 -0500

    feat: CG-0003 Added app routing module

---
+++ apps/atenea/src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'user'
+  },
+  {
+    path: 'user',
+    loadChildren: () =>
+      import('./features/user/user.module').then(m => m.UserModule)
+  },
+  {
+    path: 'learning',
+    loadChildren: () =>
+      import('./features/learning/learning.module').then(m => m.LearningModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'user'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
