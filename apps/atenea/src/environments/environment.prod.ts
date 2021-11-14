e22fbc4cbc92ad2d0f10f6e289ebcbc3e5a009d9
Cesar Gomez <cesargomez280@gmail.com>
2021-11-14 17:24:35 -0500

    feat: CG-0003 Updated environment configuration

--- apps/atenea/src/environments/environment.prod.ts
+++ apps/atenea/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ const origin = 'http://localhost:4200';
 export const environment = {
   production: true,
   apis: {
-    user: `${origin}/api/users`,
-    learning: `${origin}/api/learning`
+    user: `${origin}/api/users/`,
+    learning: `${origin}/api/learning/`
   }
 };
--- apps/atenea/src/environments/environment.ts
+++ apps/atenea/src/environments/environment.ts
@@ -3,7 +3,7 @@ const origin = 'http://localhost:4200';
 export const environment = {
   production: false,
   apis: {
-    user: `${origin}/api/users`,
-    learning: `${origin}/api/learning`
+    user: `${origin}/api/users/`,
+    learning: `${origin}/api/learning/`
   }
 };
