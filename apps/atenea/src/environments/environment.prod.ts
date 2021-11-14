381ed4bd830cf1574cb56c65c0c7d7b6111c6ff4
Cesar Gomez <cesargomez280@gmail.com>
2021-11-14 11:53:17 -0500

    fear: CG-0003 Updated environment file

--- apps/atenea/src/environments/environment.prod.ts
+++ apps/atenea/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
+const origin = 'http://localhost:4200';
+
 export const environment = {
   production: true,
+  apis: {
+    user: `${origin}/api/users`,
+    learning: `${origin}/api/learning`
+  }
 };
