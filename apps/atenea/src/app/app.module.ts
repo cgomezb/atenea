4e83ea9df5753c17754252fb59094cb8aef11cc2
Cesar Gomez <cesargomez280@gmail.com>
2021-11-15 17:45:03 -0500

    feat: CG-0003 Added angular material tools

--- apps/atenea/src/app/app.module.ts
+++ apps/atenea/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [CoreModule, AppRoutingModule],
+  imports: [CoreModule, AppRoutingModule, BrowserAnimationsModule],
   bootstrap: [AppComponent]
 })
 
