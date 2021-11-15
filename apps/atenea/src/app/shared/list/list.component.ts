7132a8ab94a0f75dfa7b5e6525568ffa83d1e011
Cesar Gomez <cesargomez280@gmail.com>
2021-11-15 12:12:09 -0500

    feat: CG-0003 Added list component

---
+++ apps/atenea/src/app/shared/list/list.component.ts
@@ -0,0 +1,10 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'atenea-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class ListComponent {}
--- apps/atenea/src/app/shared/shared.module.ts
+++ apps/atenea/src/app/shared/shared.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
 import { SearchControlComponent } from './search-control/search-control.component';
 import { FormsModule } from '@angular/forms';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
     LoaderComponent,
-    SearchControlComponent
+    SearchControlComponent,
+    ListComponent
   ],
   imports: [
     CommonModule,
@@ -15,7 +17,8 @@ import { FormsModule } from '@angular/forms';
   ],
   exports: [
     LoaderComponent,
-    SearchControlComponent
+    SearchControlComponent,
+    ListComponent
   ]
 })
 
